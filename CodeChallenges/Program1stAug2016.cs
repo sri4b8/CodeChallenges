e2bfceb170744d8b5f4acc84fdef66b34b1df856
sri4b8 <pilla_srikanth@yahooo.com>
2016-08-01 23:20:48 +0530

    Code Challange @1Aug2016

---
+++ CodeChallenges/Program1stAug2016.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges
+{
+    public class Program1stAug2016
+    {
+        public static void Main(string[] args)
+        {
+            Program1stAug2016 program = new Program1stAug2016();
+            string[] tokens = Console.ReadLine().Split(',');
+            int[] values = Array.ConvertAll(tokens, Int32.Parse);
+            bool result =program.isSame(values);
+            Console.WriteLine(result ? "YES" : "NO");
+        }
+
+        public bool isSame(int[] values)
+        {
+            int rightIndex = values.Length - 1;
+            int leftIndex = 0;
+            int leftSum = values[leftIndex];
+            int rightSum = values[rightIndex];
+            while (leftIndex != rightIndex)
+            {
+                if (leftSum < rightSum)
+                {
+                    leftIndex++;
+                    leftSum += values[leftIndex];
+
+                }
+                else
+                {
+                    rightIndex--;
+                    rightSum += values[rightIndex];
+                }
+            }
+            if (leftSum == rightSum)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CodeChallengesTest/Program1stAug2016Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenges;
+
+
+namespace CodeChallengesTest
+{
+    [TestClass]
+    public class Program1stAug2016Test
+    {  
+       Program1stAug2016  program=new Program1stAug2016();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] inputValues = new int[3] {1,2,3 };
+            bool result = program.isSame(inputValues);
+            Assert.IsFalse(result);             
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] inputValues = new int[4] { 11,20,8,31 };
+            bool result = program.isSame(inputValues);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] inputValues = new int[8] {  1,9,21,35,61,65,62,72};
+            bool result = program.isSame(inputValues);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] inputValues = new int[10] { 1,4,9,3,6,7,5,8,6,4 };
+            bool result = program.isSame(inputValues);
+            Assert.IsTrue(result);
+        }
+    }
+}
