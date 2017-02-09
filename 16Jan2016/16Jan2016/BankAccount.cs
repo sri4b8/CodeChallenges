b68b81256e2a9c9b3248e275efd4bfec2ab36aba
CAMPSYS\srikanthpilla <srikanth.pilla@campsystems.com>
2017-02-09 16:57:58 +0530

    16th Jan 2017 Code challenge

--- 16Jan2016/16Jan2016/BankAccount.cs
+++ 16Jan2016/16Jan2016/BankAccount.cs
@@ -8,7 +8,6 @@ namespace _16Jan2016
 {
     public class BankAccount :ITransaction
     {
-        public decimal InitialBalance { get; set; }
 
         public List<ITransaction> Transactions=new List<ITransaction>(); 
         public void Accept(ITransactionVisitor transactionVisitor)
